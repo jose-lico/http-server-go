82bfadf370047bb238ad6596f40a39c860976ed5
José Lico <lico.jose.pt@gmail.com>
2024-07-10 15:06:45 +0100

    Return 200

--- app/server.go
+++ app/server.go
@@ -15,9 +15,11 @@ func main() {
 		os.Exit(1)
 	}
 
-	_, err = l.Accept()
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+
+	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 }
