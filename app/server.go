b66c054f65ea28cefacabf5bc34c46a9436b5cde
José Lico <lico.jose.pt@gmail.com>
2024-07-11 15:42:52 +0100

    Removed print

--- app/server.go
+++ app/server.go
@@ -12,8 +12,6 @@ import (
 )
 
 func main() {
-	fmt.Println("Logs from your program will appear here!")
-
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
